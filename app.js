28f2c9ccad999e2a478a4f17b9594858e0920d0a
rachel williams <rachel.williams@leapdev.io>
2020-10-19 16:09:49 +1100

    CRUD

--- app.js
+++ app.js
@@ -35,8 +35,11 @@ mongoose.connect(dbConn, {
 
 app.set("view engine", "ejs");
 
-app.get('/',(req, res) => {
-    res.render('todo.ejs');
+// GET METHOD
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
+    });
     });
 
 // app.use('/posts', postRouter);
@@ -63,4 +66,30 @@ app.post('/',async (req, res) => {
     } catch (err) {
     res.redirect("/");
     }
+    });
+
+    //UPDATE
+app
+.route("/edit/:id")
+.get((req, res) => {
+const id = req.params.id;
+TodoTask.find({}, (err, tasks) => {
+res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+});
+})
+.post((req, res) => {
+const id = req.params.id;
+TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+if (err) return res.send(500, err);
+res.redirect("/");
+});
+});
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+    });
     });
